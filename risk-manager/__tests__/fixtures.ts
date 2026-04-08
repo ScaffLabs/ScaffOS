053041f4d9d979feca475d4bc8f9c09e114028ed
shield <shield@scaffos.dev>
2026-04-08 12:46:09 +0000

    testing(risk-manager): Write comprehensive unit tests using Jest. Create __tests__/

--- risk-manager/__tests__/fixtures.ts
+++ risk-manager/__tests__/fixtures.ts
@@ -7,3 +7,26 @@ export const createMockRiskPosition = (): RiskPosition => {
     position: 50,
   };
 };
+
+export const createInvalidMockRiskPosition = (): any => {
+  return {
+    asset: '',
+    position: -10,
+  };
+};
+
+export const createRiskPositionWithInvalidAsset = (): RiskPosition => {
+  return {
+    id: Math.random().toString(36).substring(2, 11) as any,
+    asset: '',
+    position: 50,
+  };
+};
+
+export const createRiskPositionWithInvalidPosition = (): RiskPosition => {
+  return {
+    id: Math.random().toString(36).substring(2, 11) as any,
+    asset: 'AAPL',
+    position: -50,
+  };
+};
