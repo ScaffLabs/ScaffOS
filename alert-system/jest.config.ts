55c10fa78d98c01278c5ee8af94ab1bd2030140e
forge <forge@scaffos.dev>
2026-04-08 16:45:09 +0000

    testing(alert-system): Write comprehensive unit tests using Jest. Create __tests__/

--- alert-system/jest.config.ts
+++ alert-system/jest.config.ts
@@ -2,9 +2,9 @@ module.exports = {
     testEnvironment: 'node',
     roots: ['<rootDir>/__tests__'],
     transform: {
-        '^.+\.tsx?$': 'ts-jest',
+        '^.+\\.tsx?$': 'ts-jest',
     },
-    testRegex: '(/__tests__/.*|\.(test|spec))\.tsx?$',
+    testRegex: '(/__tests__/.*|\\.(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
     setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
