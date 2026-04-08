b8b5e104a367f599d10fcafe93ffeb97efd73bd7
shield <shield@scaffos.dev>
2026-04-08 16:02:31 +0000

    data-layer(monitoring): Add a proper data persistence layer. Use an in-memory store

--- monitoring/seedData.ts
+++ monitoring/seedData.ts
@@ -1,10 +1,13 @@
 import InMemoryStore from './dataStore';
 
-export const seedData = (store: InMemoryStore<any>) => {
+export const seedData = (store: InMemoryStore<any>): void => {
     const sampleData = [
         { id: '1', value: 100 },
         { id: '2', value: 200 },
-        { id: '3', value: 300 }
+        { id: '3', value: 300 },
+        { id: '4', value: 400 },
+        { id: '5', value: 500 },
+        { id: '6', value: 600 },
     ];
 
     sampleData.forEach(data => {
@@ -12,15 +15,10 @@ export const seedData = (store: InMemoryStore<any>) => {
     });
 };
 
-// Function to seed additional data for testing and development
-export const seedAdditionalData = (store: InMemoryStore<any>) => {
-    const additionalData = [
-        { id: '4', value: 400 },
-        { id: '5', value: 500 },
-        { id: '6', value: 600 }
-    ];
+export const clearStore = (store: InMemoryStore<any>): void => {
+    store.clearData();
+};
 
-    additionalData.forEach(data => {
-        store.create(data, data.id);
-    });
+export const migrateStore = (sourceStore: InMemoryStore<any>, targetStore: InMemoryStore<any>): void => {
+    sourceStore.migrateData(targetStore);
 };
