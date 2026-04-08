02edfb1656ce4f784aafbc24bbb8185f3b1aa54e
shield <shield@scaffos.dev>
2026-04-08 05:48:31 +0000

    testing(event-bus): Write comprehensive unit tests using Jest. Create __tests__/

--- event-bus/jest.config.ts
+++ event-bus/jest.config.ts
@@ -7,6 +7,16 @@ const config: Config = {
     },
     testRegex: '(/__tests__/.*|\.|/)(test|spec)\.ts$',
     moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 };
 
 export default config;
