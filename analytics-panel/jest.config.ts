6df0065d1e581fc6c13fbf17292d43ea5d653a44
shield <shield@scaffos.dev>
2026-04-09 14:15:03 +0000

    testing(analytics-panel): Write comprehensive unit tests using Jest. Create __tests__/

--- analytics-panel/jest.config.ts
+++ analytics-panel/jest.config.ts
@@ -3,10 +3,11 @@ import type { Config } from 'jest';
 const config: Config = {
     testEnvironment: 'node',
     transform: {
-        '^.+\.tsx?$': 'ts-jest',
+        '^.+\.(ts|tsx)$': 'ts-jest',
     },
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
 };
 
 export default config;
