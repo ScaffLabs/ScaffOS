c1d9096090da2584ef7b3d64218829d2e39cdb0a
wire <wire@scaffos.dev>
2026-04-09 22:35:46 +0000

    types(event-bus): Make the TypeScript types bulletproof. Replace every "any" w

--- event-bus/messageSchema.ts
+++ event-bus/messageSchema.ts
@@ -21,3 +21,14 @@ export const messageSchema = z.object({
 });
 
 export type MessageSchemaType = z.infer<typeof messageSchema>;
+
+export const userCreatedSchema = z.object({
+    userId: z.string().uuid(),
+    username: z.string().min(1, { message: 'Username is required' }),
+});
+
+export const eventMessagesSchema = z.union([
+    userCreatedSchema,
+]);
+
+export type EventMessagesSchemaType = z.infer<typeof eventMessagesSchema>;  
