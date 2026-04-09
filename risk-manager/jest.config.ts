a9ee54e375e51280666922686bdd80d65dba3751
shield <shield@scaffos.dev>
2026-04-09 20:32:43 +0000

    testing(risk-manager): Write comprehensive unit tests using Jest. Create __tests__/

--- risk-manager/jest.config.ts
+++ risk-manager/jest.config.ts
@@ -1,15 +1,1 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['<rootDir>/dist/'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  collectCoverage: true,
-  coverageDirectory: '<rootDir>/coverage',
-  coverageReporters: ['text', 'lcov'],
-  globals: {
-    'ts-jest': {
-      diagnostics: false,
-    },
-  },
-  setupFiles: ['dotenv/config'],
-};
+module.exports = {preset: 'ts-jest', testEnvironment: 'node', testPathIgnorePatterns: ['<rootDir>/dist/'], setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], collectCoverage: true, coverageDirectory: '<rootDir>/coverage', coverageReporters: ['text', 'lcov'], globals: {'ts-jest': {diagnostics: false,},}, setupFiles: ['dotenv/config'],};
