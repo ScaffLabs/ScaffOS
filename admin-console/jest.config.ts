1b7a40552aa304dbf68ed8cf3d80811f9fbc0078
shield <shield@scaffos.dev>
2026-04-10 02:27:06 +0000

    testing(admin-console): Write comprehensive unit tests using Jest. Create __tests__/

--- admin-console/jest.config.ts
+++ admin-console/jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom',
     transform: {
         '^.+\.(ts|tsx)$': 'ts-jest',
     },
