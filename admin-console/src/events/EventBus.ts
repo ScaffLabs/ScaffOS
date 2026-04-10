4a9df70bf1f7d37886e87c4fd4adebf3de070a39
shield <shield@scaffos.dev>
2026-04-10 07:06:48 +0000

    wiring(admin-console): Make the inter-service communication ACTUALLY WORK. Add real

--- admin-console/src/events/EventBus.ts
+++ admin-console/src/events/EventBus.ts
@@ -1,17 +1,27 @@
 import { EventEmitter } from 'events';
 import { AppEvent } from '../types';
+import winston from 'winston';
 
 const eventBus = new EventEmitter();
 
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [new winston.transports.Console()],
+});
+
 export const emitEvent = (event: string, data: any) => {
+    logger.info(`Emitting event: ${event}`, { data });
     eventBus.emit(event, data);
 };
 
 export const subscribeToEvent = (event: string, listener: (data: any) => void) => {
+    logger.info(`Subscribing to event: ${event}`);
     eventBus.on(event, listener);
 };
 
 export const unsubscribeFromEvent = (event: string, listener: (data: any) => void) => {
+    logger.info(`Unsubscribing from event: ${event}`);
     eventBus.off(event, listener);
 };
 
@@ -19,9 +29,9 @@ export const getEventBus = () => eventBus;
 
 // Initialize listeners for events
 subscribeToEvent('CONFIGURATION_DELETED', (data) => {
-    console.log('Configuration deleted:', data);
+    logger.info('Configuration deleted:', data);
 });
 
 subscribeToEvent('CONFIGURATION_CREATED', (data) => {
-    console.log('Configuration created:', data);
+    logger.info('Configuration created:', data);
 });
